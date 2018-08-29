36c3aa0209ec048775ba777f049525e1fc7e5dc4
Rye <ryexsmith@googlemail.com>
2018-08-29 14:11:07 +0100

    Modal content change

--- index.php
+++ index.php
@@ -292,15 +292,12 @@
    									 <header class="modal-card-head">
      								 	<p style="color: #5a5a5a !important; font-family: adam;" class="modal-card-title">Ellis Collinson</p>
      								 	<button id="Ellisclose" class="delete" aria-label="close"></button>
-    								</header>
-									<section class="modal-card-body">
-									<p id="ModalDescription" class="modal-card-title">I'm a Front-end developer and the Lead Graphic Designer for Sicarius. I've designed all of Sicarius' assets and I confirm that every design I produce is complete to a perfect standard</p>
-									</section>
-									<section class="modal-card-body">
-									<p id="ModalDescription" class="modal-card-title">I'm a Front-end developer and the Lead Graphic Designer for Sicarius. I've designed all of Sicarius' assets and I confirm that every design I produce is complete to a perfect standard</p>
-									</section>
+									</header>
 									<section class="modal-card-body">
 										<img src="images/EllisPolaroid.png">
+									</section>
+									<section class="modal-card-body">
+										<p id="ModalDescription" class="modal-card-title">I'm a Front-end developer and the Lead Graphic Designer for Sicarius. I've designed all of Sicarius' assets and I confirm that every design I produce is complete to a perfect standard</p>
 									</section>
 								  </div>
 						</div>
