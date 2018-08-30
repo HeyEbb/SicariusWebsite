c992fca3a6f852cd370b7b3d154211f676996ae4
Rye <RyexSmith@googlemail.com>
2018-08-30 11:09:40 +0100

    Modal content change

--- index.php
+++ index.php
@@ -291,17 +291,15 @@
   								<div style="width: 400px; border-radius: 5px;" class="modal-card">
    									 <header class="modal-card-head">
      								 	<p style="color: #5a5a5a !important; font-family: adam;" class="modal-card-title">Ellis Collinson</p>									
-     								 	<button id="Ellisclose" class="delete" aria-label="close"></button>									
+     								 	<button id="Ellisclose" class="delete" aria-label="close"><br /></button>
+										<p style="color: #5a5a5a !important; font-family: adamlight; font-size: 1em;" class="modal-card-title">Creative Director</p>									
 									</header>
-									<section style="padding:0px;" class="modal-card-body">
-										<p style="color: #5a5a5a !important; font-family: adamlight; font-size: 1.2em;" class="modal-card-title">Creative Director</p>
-									</section>
+									
 									<section style="padding:0px;" class="modal-card-body">
 										<img style="height:270px; display: block; margin-left: auto; margin-right: auto;" src="images/RyanModal.png">
 									</section>
 									<section class="modal-card-body">
 										<p style="color: #5a5a5a !important; font-family: adam; font-size: 1.2em;" class="modal-card-title">Role</p>
-										<br>
 										<p id="ModalDescription" class="modal-card-title">I'm a Front-end developer and the Lead Graphic Designer for Sicarius. I've designed all of Sicarius' assets and I confirm that every design I produce is complete to a perfect standard</p>		
 									</section>
 									<section class="modal-card-body">
