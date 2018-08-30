d47074e693ef8f2e2de0362b864ed3b887502fa9
Rye <RyexSmith@googlemail.com>
2018-08-30 11:10:33 +0100

    Modal line break in header

--- index.php
+++ index.php
@@ -291,7 +291,7 @@
   								<div style="width: 400px; border-radius: 5px;" class="modal-card">
    									 <header class="modal-card-head">
      								 	<p style="color: #5a5a5a !important; font-family: adam;" class="modal-card-title">Ellis Collinson</p>									
-     								 	<button id="Ellisclose" class="delete" aria-label="close"><br /></button>
+     								 	<button id="Ellisclose" class="delete" aria-label="close"></button><br />
 										<p style="color: #5a5a5a !important; font-family: adamlight; font-size: 1em;" class="modal-card-title">Creative Director</p>									
 									</header>
 									
